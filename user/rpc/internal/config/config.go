2dd250f97eec1c32282d774b29a98ed6a1d4bcd1
SM1701 <717513736@qq.com>
2021-11-19 16:26:44 +0800

    update

--- user/rpc/internal/config/config.go
+++ user/rpc/internal/config/config.go
@@ -1,7 +1,12 @@
 package config
 
-import "github.com/tal-tech/go-zero/zrpc"
+import (
+	"github.com/tal-tech/go-zero/core/stores/cache"
+	"github.com/tal-tech/go-zero/zrpc"
+)
 
 type Config struct {
 	zrpc.RpcServerConf
+	Cache      cache.CacheConf
+	DataSource string
 }
--- user/rpc/internal/svc/servicecontext.go
+++ user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,29 @@
 package svc
 
-import "zero-mall/zero_bbs/user/rpc/internal/config"
+import (
+	"errors"
+	"github.com/tal-tech/go-zero/core/stores/cache"
+	"github.com/tal-tech/go-zero/core/stores/sqlx"
+	"github.com/tal-tech/go-zero/core/syncx"
+	"zero-mall/zero_bbs/user/model"
+	"zero-mall/zero_bbs/user/rpc/internal/config"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UsersModel
+	Cache     cache.Cache
 }
 
+var ErrNotFound = errors.New("data not find")
+
 func NewServiceContext(c config.Config) *ServiceContext {
+
+	ca := cache.New(c.Cache, syncx.NewSingleFlight(), cache.NewStat("dc"), ErrNotFound)
+
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUsersModel(sqlx.NewMysql(c.DataSource), c.Cache),
+		Cache:     ca,
 	}
 }
