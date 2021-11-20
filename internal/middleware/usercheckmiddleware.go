7ee7d0771465ead967dd4ea250d409b2b93ff3d0
SM1701 <717513736@qq.com>
2021-11-20 21:53:08 +0800

    使用中间件校验用户 uid

--- internal/middleware/usercheckmiddleware.go
+++ internal/middleware/usercheckmiddleware.go
@@ -1,6 +1,21 @@
 package middleware
 
-import "net/http"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/tal-tech/go-zero/rest/httpx"
+	"net/http"
+)
+
+var (
+	errorUserInfo = errors.New("用户信息获取失败")
+	authDeny      = errors.New("用户信息不一致")
+)
+
+const (
+	userKey = `x-user-id`
+)
 
 type UserCheckMiddleware struct {
 }
@@ -11,9 +26,24 @@ func NewUserCheckMiddleware() *UserCheckMiddleware {
 
 func (m *UserCheckMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO generate middleware implement function, delete after code implementation
+		userId := r.Header.Get(userKey)
+		jwtUserId := r.Context().Value("uid")
+		userInt, err := json.Number(userId).Int64()
+		if err != nil {
+			httpx.Error(w, errorUserInfo)
+			return
+		}
 
-		// Passthrough to next handler if need
+		jwtInt, err := json.Number(fmt.Sprintf("%v", jwtUserId)).Int64()
+		if err != nil {
+			httpx.Error(w, errorUserInfo)
+			return
+		}
+
+		if jwtInt != userInt {
+			httpx.Error(w, authDeny)
+			return
+		}
 		next(w, r)
 	}
 }
